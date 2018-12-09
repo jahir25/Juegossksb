ae1b56c642e29f3b0f8b37bede4e8b0368cdf57d
jahir25 <jahirdiaz2595@gmail.com>
2018-12-09 13:11:33 -0500

    ComentarioJahir

--- assets/js/juegossksbProducts.js
+++ assets/js/juegossksbProducts.js
@@ -81,68 +81,69 @@ function onCreateElement(data, result){
 			divtitle.appendChild(h2);
 			document.getElementById("idRow").appendChild(divtitle);
 
-			$.each(result, function(k, v){	
+			$.each(result, function(k, v){
+				
+					if(va == v.category){
+							/*ITEM SUB CATALOGO*/
 
-					/*ITEM SUB CATALOGO*/
-
-					random = Math.floor(Math.random() * 6)+1;
-					bgcolor = "bg-color-" + random;
-					borcolor = "border-color-" + random;
-
-					var div = document.createElement("div");
-					div.classList.add("col-md-3");
-					div.classList.add("col-xs-6");
-
-					var div2 = document.createElement("div");
-					div2.classList.add("box");
-					div2.classList.add(bgcolor);
-
-					var div3 = document.createElement("div");
-					div3.classList.add("box-img");
-					div3.classList.add(borcolor);
-					div3.classList.add("text-center");
-
-
-					/*=======================*/
-					var encrypted = CryptoJS.AES.encrypt(data.ca_id + "/" + v.id, "P4ssW0rJu3g0sKs3.com");
-					var a = document.createElement("a");
-					a.setAttribute("href", "details.html?" + encrypted);
-					var img = document.createElement("img");
-					img.classList.add("img-responsive");
-					img.setAttribute("alt", "image");
-					img.setAttribute("src", v.assets);
-					
-					var aclick = document.createElement("a");
-					// aclick.setAttribute("onclick", "onModal(this,titu)");
-					// aclick.addEventListener("click", onModal(this));
-					aclick.setAttribute("href", "details.html?" + encrypted);
-					aclick.setAttribute("data-toggle", "modal");
-					aclick.setAttribute("id", v.id);
-					aclick.classList.add("btn-block")
-					var i = document.createElement("i");
-					i.setAttribute("aria-hidden", "true");
-					i.classList.add("fa");
-					i.classList.add("fa-sign-in");
-					aclick.innerHTML = "Visualizar ";
-					var div3$1 = document.createElement("div");
-					div3$1.classList.add("box-info");
-					var h4 = document.createElement("h4");
-					h4.innerHTML = v.titulo;
-
-					div3$1.appendChild(h4);
-
-					aclick.appendChild(i);
-					a.appendChild(img);
-
-					div3.appendChild(a);
-					div3.appendChild(aclick);
-					/*=======================*/
-					div2.appendChild(div3);
-					div2.appendChild(div3$1);
-					div.appendChild(div2);
-
-					document.getElementById("idRow").appendChild(div);		
-
+							random = Math.floor(Math.random() * 6)+1;
+							bgcolor = "bg-color-" + random;
+							borcolor = "border-color-" + random;
+	
+							var div = document.createElement("div");
+							div.classList.add("col-md-3");
+							div.classList.add("col-xs-6");
+	
+							var div2 = document.createElement("div");
+							div2.classList.add("box");
+							div2.classList.add(bgcolor);
+	
+							var div3 = document.createElement("div");
+							div3.classList.add("box-img");
+							div3.classList.add(borcolor);
+							div3.classList.add("text-center");
+	
+	
+							/*=======================*/
+							var encrypted = CryptoJS.AES.encrypt(data.ca_id + "/" + v.id, "P4ssW0rJu3g0sKs3.com");
+							var a = document.createElement("a");
+							a.setAttribute("href", "details.html?" + encrypted);
+							var img = document.createElement("img");
+							img.classList.add("img-responsive");
+							img.setAttribute("alt", "image");
+							img.setAttribute("src", v.assets);
+							
+							var aclick = document.createElement("a");
+							// aclick.setAttribute("onclick", "onModal(this,titu)");
+							// aclick.addEventListener("click", onModal(this));
+							aclick.setAttribute("href", "details.html?" + encrypted);
+							aclick.setAttribute("data-toggle", "modal");
+							aclick.setAttribute("id", v.id);
+							aclick.classList.add("btn-block")
+							var i = document.createElement("i");
+							i.setAttribute("aria-hidden", "true");
+							i.classList.add("fa");
+							i.classList.add("fa-sign-in");
+							aclick.innerHTML = "Visualizar ";
+							var div3$1 = document.createElement("div");
+							div3$1.classList.add("box-info");
+							var h4 = document.createElement("h4");
+							h4.innerHTML = v.titulo;
+	
+							div3$1.appendChild(h4);
+	
+							aclick.appendChild(i);
+							a.appendChild(img);
+	
+							div3.appendChild(a);
+							div3.appendChild(aclick);
+							/*=======================*/
+							div2.appendChild(div3);
+							div2.appendChild(div3$1);
+							div.appendChild(div2);
+	
+							document.getElementById("idRow").appendChild(div);
+					}					
 			});
 		});		
 	}else {
